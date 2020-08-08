08e22e859fc222a1803d6396a14176d2878181ba
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-08-08 21:31:12 +0530

    Create max and min of an array.java

---
+++ Array/max and min of an array/max and min of an array.java
@@ -0,0 +1,27 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		while(t-->0)
+		{
+		    int n= sc.nextInt();
+		    int arr[]= new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextInt();
+		    }
+		    
+		   Arrays.sort(arr);
+		   System.out.println(arr[n-1] + " " +arr[0]);
+		      
+		    
+		}
+	}
+}
