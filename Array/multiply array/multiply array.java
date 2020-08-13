903de2d0ea9288c08d6eb643b0c167034c6da3c3
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-08-13 12:59:34 +0530

    Create multiply array.java

---
+++ Array/multiply array/multiply array.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+	public static void main (String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    int t=scan.nextInt();
+	    while(t -->0){
+	        
+	        int n= scan.nextInt();
+	        int[] arr = new int[n];
+	        for(int i=0;i<n;i++){
+	            arr[i] = scan.nextInt();
+	        }
+	       int x=1;
+	        for(int i=0;i<n;i++)
+	        {
+	            x=x*arr[i];
+	            
+	        }
+	       System.out.println(x);
+	       
+	    }
+	
+
+	}
+}
