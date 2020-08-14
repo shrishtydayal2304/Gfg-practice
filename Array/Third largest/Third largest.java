271b4be8a72401fa5e19f05842fd0c31cc22449d
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-08-14 13:32:38 +0530

    Create Third largest.java

---
+++ Array/Third largest/Third largest.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+class ThirdLargestElement
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n =sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.thirdLargest(arr,n));
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+class GfG
+{
+    int thirdLargest(int a[], int n)
+    {
+Arrays.sort(a);
+	    
+	   if(n>=3)
+	    return a[n-3];
+	    else return -1;
+}
+}
