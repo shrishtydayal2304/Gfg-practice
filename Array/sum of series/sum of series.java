48ba9b420c8e7fbe4837e117ebd535dbda085d44
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-08-16 13:30:56 +0530

    Create sum of series.java

---
+++ Array/sum of series/sum of series.java
@@ -0,0 +1,25 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc =new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+		    int n =sc.nextInt();
+		    
+		    int sum=0;
+		    for(int i=0;i<=n;i++){
+		        sum = (n*(n+1))/2;
+		      
+		    }
+		    
+		    System.out.println(sum);  
+		}
+	}
+}
