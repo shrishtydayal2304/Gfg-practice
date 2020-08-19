d9582611545dcb6465bf72666bbdb3a360ec0fea
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-08-19 19:11:20 +0530

    Create convert string to lower case.java

---
+++ String/convert string to lowercase/convert string to lower case.java
@@ -0,0 +1,18 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		while(t-->0)
+		{
+		    String s = sc.next();
+		   System.out.println(s.toLowerCase());
+		    }
+		}
+	}
