1a5c528d022c0c690801255eb7ab3c8bae351c12
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-08-22 16:22:40 +0530

    Create java substring.java

---
+++ String/java substring/java substring.java
@@ -0,0 +1,24 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+	while(t-->0)
+	{
+	    String s = sc.next();
+	    int l=sc.nextInt();
+	    int r =sc.nextInt();
+	 
+	    
+	        System.out.println(s.substring(l,r+1));
+	    
+	  
+	}
+	}
+}
