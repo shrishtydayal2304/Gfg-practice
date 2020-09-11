c1b738c98bbab9ff915a98516ee9a589c497f9ee
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-09-11 12:28:15 +0530

    Create Stack designer.java

---
+++ Stack/Stack designer/Stack designer.java
@@ -0,0 +1,66 @@
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    //Creating an ArrayList
+		    ArrayList<Integer> arr = new ArrayList<>();
+		    
+		    //Taking input the total number of elements
+		    int n = sc.nextInt();
+		    
+		    //adding all the elements to the ArrayList
+		    for(int i=0;i<n;i++)
+		    {
+		        int x = sc.nextInt();
+		        arr.add(x);
+		    }
+		    
+		    //Calling the push method and passing 
+		    //ArrayList and the it's size
+		    Stack<Integer>mys = _push(arr,n);
+		    
+		    //Calling the pop method
+		    //and passing Stack
+		    _pop(mys);
+		    
+		    System.out.println();
+		}
+	}
+	
+	
+ // } Driver Code Ends
+
+
+//User function Template for Java
+
+
+public static Stack<Integer>_push(ArrayList<Integer> arr,int n)
+{
+    Stack<Integer> st = new Stack<>();
+    for(int a:arr){
+        st.push(a);
+    }
+    return st;
+}
+
+public static void _pop(Stack<Integer>s)
+{
+    while(!s.isEmpty()){
+        System.out.print(s.pop()+" ");
+    }
+}
+
+// { Driver Code Starts.
+}  // } Driver Code Ends
